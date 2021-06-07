3f76389ce1dbacf9d09600d9d6179c868c8c07f8
Tatyana Doni <tatyana.donirofman@gmail.com>
2021-06-07 12:02:35 +0300

    GeneralController was created

---
+++ src/main/java/com/telran/generalPage/controller/HomePageController.java
@@ -0,0 +1,19 @@
+package com.telran.generalPage.controller;
+
+import com.telran.generalPage.dto.Application;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+public class HomePageController {
+
+    @GetMapping("applications")
+    @Bean
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<Application> GetAllProductsOfCompany(){
+        return List.of(Application.builder().id(123).name("Math").build());
+    }
+}
