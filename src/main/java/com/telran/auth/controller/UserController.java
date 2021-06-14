92e8602c77ef9171a741e68c9a97eb3d7893e1ee
Tatyana Doni <tatyana.donirofman@gmail.com>
2021-06-14 18:24:06 +0300

    Changing in method user/{userEmail}

--- src/main/java/com/telran/auth/controller/UserController.java
+++ src/main/java/com/telran/auth/controller/UserController.java
@@ -1,15 +1,22 @@
 package com.telran.auth.controller;
 
+import com.telran.auth.dao.UserCredentialsEntity;
 import com.telran.auth.dao.UserCredentialsRepo;
 import com.telran.auth.dao.UserProfileEntity;
 import com.telran.auth.dto.UserProfileDto;
 import com.telran.auth.service.UserProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotNull;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("user")
@@ -18,10 +25,13 @@ public class UserController {
 
     UserProfileService profileService;
     UserCredentialsRepo credentialsRepo;
+    @Value("${admin}")
+    String adminName;
 
     @Autowired
-    public UserController(UserProfileService profileService,UserCredentialsRepo credentialsRepo){
-        this.profileService =profileService;
+    public UserController(UserProfileService profileService,
+                          UserCredentialsRepo credentialsRepo){
+        this.profileService=profileService;
         this.credentialsRepo=credentialsRepo;
     }
 
@@ -51,7 +61,17 @@ public class UserController {
     }
 //    @RolesAllowed({"ADMIN","STUDENT"})
     @GetMapping("{userEmail}")
-    public UserProfileDto getUserByEmail(@NotNull @PathVariable String userEmail){
+    public UserProfileDto getUserByEmail(@NotNull @PathVariable String userEmail,
+                                         HttpServletRequest request){
+//**********Checking if userCredential's email equals to pathVariable email, but ADMIN can get info*************
+        if (!request.getUserPrincipal().getName().equals(userEmail)){
+            System.out.println(request.getUserPrincipal().getName());
+            System.out.println(adminName);
+            if (!request.getUserPrincipal().getName()
+                    .equals(adminName)) {
+                throw new RuntimeException("You can't get this user profile");
+            }
+        }
         UserProfileEntity entity = profileService.getUser(userEmail);
         return UserProfileDto.builder()
                 .id(entity.getId())
